a7a4d572e11606cb8b73f5b17286c09878d0da5f
Jordan Belinsky <jordanbelinsky1@gmail.com>
2021-09-27 12:26:58 -0400

    Edited comments

--- product.py
+++ product.py
@@ -9,9 +9,9 @@ db = SQLAlchemy(app)
 class Product(db.Model):
     product_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    condition = db.Column(db.String(60), nullable=False)  # deal with this later! should be selection not string
+    condition = db.Column(db.String(60), nullable=False)  # maybe should be selection not string?
     seller = db.Column(db.String(50), unique=True, nullable=False)
     modelNum = db.Column(db.Integer)
-    color = db.Column(db.String(60), nullable=False)  # deal with this later! maybe should be selection not string
-    category = db.Column(db.String(60), nullable=False)  # deal with this later! should be selection not string
+    color = db.Column(db.String(60), nullable=False)
+    category = db.Column(db.String(60), nullable=False)  # maybe should be selection not string?
     description = db.Column(db.String(1500), nullable=False)
