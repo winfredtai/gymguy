52763c54682635764a8a88e4ba2a131a4eddb9aa
Jordan Belinsky <jordanbelinsky1@gmail.com>
2021-09-27 14:23:50 -0400

    Fix flake8 formatting

--- product.py
+++ product.py
@@ -21,19 +21,22 @@ Product Class Data Model
 class Product(db.Model):
     # product ID which will be the key by which all other data can be found
     product_id = db.Column(db.Integer, primary_key=True)
-    # product name which is mandatory, currently set to a max 80 chars aligning with eBay
+    # product name which is mandatory, max 80 chars aligning with eBay
     name = db.Column(db.String(80), nullable=False)
-    # condition of the product which is mandatory, currently set to max 60 chars
-    condition = db.Column(db.String(60), nullable=False)  # **maybe should be selection not string?**
-    # seller of the product which is mandatory, and must be a unique string, currently set to 50 chars
+    # condition of the product which is mandatory, max 60 chars
+    # **maybe should be selection not string?**
+    condition = db.Column(db.String(60), nullable=False)
+    # seller of the product which is mandatory, and must be a unique string,
+    # max 50 chars
     seller = db.Column(db.String(50), unique=True, nullable=False)
     # model # of the product which not mandatory
     modelNum = db.Column(db.Integer)
-    # colour of the product which is mandatory, currently set to max 40 chars
+    # colour of the product which is mandatory, max 40 chars
     colour = db.Column(db.String(40), nullable=False)
-    # category of the product which is mandatory, currently set to max 60 chars
-    category = db.Column(db.String(60), nullable=False)  # **maybe should be selection not string?**
-    # product description which is mandatory, currently set to max 1500 chars
+    # category of the product which is mandatory, max 60 chars
+    # **maybe should be selection not string?**
+    category = db.Column(db.String(60), nullable=False)
+    # product description which is mandatory, max 1500 chars
     description = db.Column(db.String(1500), nullable=False)
 
     def __repr__(self):
