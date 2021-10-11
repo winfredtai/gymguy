015436c36c2f82f05831f1ffac2d0dc02a9a24b2
Jordan Belinsky <jordanbelinsky1@gmail.com>
2021-10-10 21:23:30 -0400

    Added login and update_user

--- qbay/models.py
+++ qbay/models.py
@@ -1,3 +1,4 @@
+import re
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
 
@@ -90,13 +91,72 @@ class Transcation(db.Model):
 db.create_all()
 
 
+def check_username(username):
+    '''
+    Checks that username meets requirements
+      Parameters:
+        username (string):  user username
+      Returns:
+        True if username meets requirements otherwise False
+    '''
+    if not username:
+        print("Username cannot be empty.")
+        return False
+
+    if username.startswith(" ", 0) or username.endswith(" "):
+        print("Username cannot begin or end with a space.")
+        return False
+
+    if not username.replace(" ", "").isalnum():
+        print("Username can only have letters and numbers.")
+        return False
+
+    if len(username) < 2 or len(username) > 20:
+        print("Username must be between 2 and 20 characters.")
+        return False
+
+    return True
+
+
+def check_shipping_address(shipping_address):
+    '''
+    Checks that shipping address meets requirements
+      Parameters:
+        shipping_address (string):  user shipping address
+      Returns:
+        True if address meets requirements otherwise False
+    '''
+    if not shipping_address:
+        print("The shipping address must not be empty.")
+        return False
+    if not shipping_address.replace(" ", "").isalnum():
+        print("The shipping address can only have letters and numbers.")
+        return False
+    return True
+
+
+def check_postal_code(postal_code):
+    '''
+    Checks that postal code meets requirements
+      Parameters:
+        postal_code (string):   user postal code
+      Returns:
+        True if postal code meets requirements otherwise False
+    '''
+    if not re.fullmatch(r'\b[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z]\s'
+                        r'\d[ABCEGHJ-NPRSTV-Z]\d\b', postal_code):
+        print("This is not a valid postal code.")
+        return False
+    return True
+
+
 def register(name, email, password):
     '''
     Register a new user
       Parameters:
-        name (string):     user name
-        email (string):    user email
-        password (string): user password
+        name (string):      user name
+        email (string):     user email
+        password (string):  user password
       Returns:
         True if registration succeeded otherwise False
     '''
@@ -124,7 +184,37 @@ def login(email, password):
       Returns:
         The user object if login succeeded otherwise None
     '''
-    valids = User.query.filter_by(email=email, password=password).all()
-    if len(valids) != 1:
+    if check_email(email) is False:
         return None
-    return valids[0]
+    if check_password(password) is False:
+        return None
+    user = User.query.filter_by(email=email).first()
+    if user.password == password:
+        return user
+    return None
+
+
+def update_user(email, username, shipping_address, postal_code):
+    '''
+    Updates an existing user
+      Parameters:
+        email (string):             user email
+        username (string):          user username
+        shipping_address (string):  user shipping address
+        postal_code (string):       user postal code
+      Returns:
+        True if user update succeeded otherwise False
+    '''
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return False
+    username_verification = check_username(username)
+    shipping_address_verification = check_shipping_address(shipping_address)
+    postal_code_verification = check_postal_code(postal_code)
+    if username_verification and shipping_address_verification \
+            and postal_code_verification:
+        user.username = username
+        user.shipping_address = shipping_address
+        user.postal_code = postal_code
+    db.session.commit()
+    return True
--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -1,4 +1,4 @@
-from qbay.models import register, login
+from qbay.models import register, login, update_user
 
 
 def test_r1_7_user_register():
@@ -13,9 +13,9 @@ def test_r1_7_user_register():
 
 def test_r2_1_login():
     '''
-    Testing R2-1: A user can log in using her/his email address 
+    Testing R2-1: A user can log in using her/his email address
       and the password.
-    (will be tested after the previous test, so we already have u0, 
+    (will be tested after the previous test, so we already have u0,
       u1 in database)
     '''
 
@@ -25,3 +25,80 @@ def test_r2_1_login():
 
     user = login('test0@test.com', 1234567)
     assert user is None
+
+
+def test_r2_2_login():
+    '''
+    Testing R2-2: A user's inputs match the requirements.
+    '''
+
+    user = login('he', 'hello')
+    assert user is None
+
+    user = login('he.hey', 'HELLO$')
+    assert user is None
+
+
+def test_r3_1_update_user():
+    '''
+    Testing R3-1: A user can update their username,
+    shipping address and postal code.
+    '''
+
+    assert update_user(email="testuser1@gmail.com", username="testuser1",
+                       shipping_address="33 Evergreen Terrace",
+                       postal_code="L5C 2B5") is True
+    assert update_user(email="email", username=" ",
+                       shipping_address="33 Evergreen Terrace!",
+                       postal_code="83053") is False
+
+
+def test_r3_2_update_user():
+    '''
+    Testing R3-2: Updated shipping address must be
+    non empty, alphanumeric, and not contain symbols.
+    '''
+
+    assert update_user(email="testuser2@gmail.com", username="testuser2",
+                       shipping_address="33 Evergreen Terrace",
+                       postal_code="L5C 2B5") is True
+    assert update_user(email="testuser2@gmail.com", username="testuser2",
+                       shipping_address=" ",
+                       postal_code="L5C 2B5") is False
+    assert update_user(email="testuser2@gmail.com", username="testuser2",
+                       shipping_address="33 Evergr33n Terrace!",
+                       postal_code="L5C 2B5") is False
+
+
+def test_r3_3_update_user():
+    '''
+    Testing R3-3: Updated postal code must be a valid
+    Canadian Postal code.
+    '''
+
+    assert update_user(email="testuser3@gmail.com", username="testuser3",
+                       shipping_address="33 Evergreen Terrace",
+                       postal_code="L5C 2B5") is True
+    assert update_user(email="testuser3@gmail.com", username="testuser3",
+                       shipping_address="33 Evergreen Terrace",
+                       postal_code="L55 2B88") is False
+
+
+def test_r3_4_update_user():
+    '''
+    Testing R3-4: Updated username follows given
+    requirements.
+    '''
+
+    assert update_user(email="testuser4@gmail.com", username="testuser4",
+                       shipping_address="33 Evergreen Terrace",
+                       postal_code="L5C 2B5") is True
+    assert update_user(email="testuser4@gmail.com", username=" testuser4 ",
+                       shipping_address="33 Evergreen Terrace",
+                       postal_code="L55 2B88") is False
+    assert update_user(email="testuser4@gmail.com", username="t4",
+                       shipping_address="33 Evergreen Terrace",
+                       postal_code="L55 2B88") is False
+    assert update_user(email="testuser4@gmail.com", username=" ",
+                       shipping_address="33 Evergreen Terrace",
+                       postal_code="L55 2B88") is False
