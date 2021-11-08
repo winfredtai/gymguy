42eb70960f792055d1f325b660f3daa5d20f9337
jakki-Guan <17sg46@queensu.ca>
2021-11-07 21:05:44 -0500

    adding testCreateproduct test cases

---
+++ qbay_test/frontend/test_createproduct/test_createproduct_R4_1/test_createproduct_R4_1(3).py
@@ -0,0 +1,27 @@
+from os import popen
+from pathlib import Path
+import subprocess
+
+# get expected input/output file
+current_folder = Path(__file__).parent
+
+# read expected in/out
+expected_in = open(current_folder.joinpath(
+    'test_createproduct_R4_1(3).in'))
+expected_out = open(current_folder.joinpath(
+    'test_createproduct_R4_1(3).out')).read()
+
+print(expected_out)
+
+
+def test_register():
+    """capsys -- object created by pytest to
+    capture stdout and stderr"""
+
+    # pip the input
+    output = subprocess.run(
+        ['python', '-m', 'qbay'],
+        stdin=expected_in,
+        capture_output=True,
+    ).stdout.decode()
+    assert output.strip() == expected_out.strip()
